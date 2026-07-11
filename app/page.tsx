f9137e85d6ab9d55dc5eee5910eccf4fdfe1306b
matildatechacademy-lang <matildatechacademy@gmail.com>
2026-07-11 16:06:59 +0100

    Add Homepage

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: '50px', textAlign: 'center'}}>
+      <h1>MTA Junior App 🚀</h1>
+      <p>It works! Deployed from my phone!</p>
+    </main>
+  )
+}
